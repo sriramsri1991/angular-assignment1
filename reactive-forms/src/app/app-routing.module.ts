443b04c0c5b8621bf7deb12151c5593972955fb9
Sriram Sridharan <sriram.sridharan@klm.com>
2020-07-05 09:15:10 +0200

    Assignment1: Resolved caching error

--- reactive-forms/src/app/app-routing.module.ts
+++ reactive-forms/src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [{path: '', redirectTo: '/login', pathMatch: 'full'},
                         {path: 'logout', component: LogoutComponent}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
